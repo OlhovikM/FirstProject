c5768e330ccab43ff81fa8ad87fc58a7ac088b67
OlhovikM <Olhovikmaria14@gmail.com>
2021-08-20 00:14:35 +0300

    Init

---
+++ com/company/mki3/CallHello.java
@@ -0,0 +1,10 @@
+package com.company.mki3;
+import java.util.Scanner;
+public class CallHello {
+    private static void main(String[] args) {
+ System.out.println("Hello!");
+ Scanner sc=new Scanner(System.in);
+ String name=sc.nextLine();
+ System.out.print(name);
+    }
+}
