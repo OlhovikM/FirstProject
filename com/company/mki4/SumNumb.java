512b95485356be1d23d12a3898c7c1a9443f0228
OlhovikM <Olhovikmaria14@gmail.com>
2021-08-25 23:28:39 +0300

    Init

--- com/company/mki4/SumNumb.java
+++ com/company/mki4/SumNumb.java
@@ -1,4 +1,8 @@
 package com.company.mki4;
 public class SumNumb {
+    public static void main(String[] args) {
+       int sum1=14; int sum2=10;
+       System.out.print(sum1+ "+" +  sum2+"=" +sum1+sum2);
+    }
 }
 
