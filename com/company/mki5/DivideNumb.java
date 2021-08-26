d57a2f8fdf06f4cf41d5baf13d9521d7e02b29b1
OlhovikM <Olhovikmaria14@gmail.com>
2021-08-26 23:31:33 +0300

    Init

--- com/company/mki5/DivideNumb.java
+++ com/company/mki5/DivideNumb.java
@@ -1,7 +1,10 @@
 package com.company.mki5;
 public class DivideNumb {
     public static void main(String[] args) {
-        int x=15; int y=5;
-        System.out.print(x+"/"+y+"="+x/y);
+ printDevide();
+    }
+    public static void printDevide() {
+        int numberone=100; int numbertwo=5;
+        System.out.println("100/5"+"="+(numberone/numbertwo));
     }
 }
