7d13fc05029c2fa098a44fbea3afe8848f0acd32
OlhovikM <Olhovikmaria14@gmail.com>
2021-08-26 23:46:00 +0300

    Init

---
+++ com/company/mki6/ProductNumb.java
@@ -0,0 +1,10 @@
+package com.company.mki6;
+public class ProductNumb {
+    public static void main(String[] args) {
+        printProduct();
+    }
+    public static void printProduct() {
+        int numberone=25; int numbertwo=5;
+        System.out.println("25*5"+"="+(numberone*numbertwo));
+    }
+}
