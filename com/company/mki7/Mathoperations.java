2308c061171fb794f91acb5ace93c8b9bdd6a1a1
OlhovikM <Olhovikmaria14@gmail.com>
2021-08-26 23:53:58 +0300

    Init

---
+++ com/company/mki7/Mathoperations.java
@@ -0,0 +1,15 @@
+package com.company.mki7;
+public class Mathoperations {
+    public static void main(String[] args) {
+        printNumbers();
+    }
+    public static void printNumbers() {
+        int numberone=125; int numbertwo=24;
+        System.out.println("125+24"+"="+(numberone+numbertwo));
+        System.out.println("125-24"+"="+(numberone-numbertwo));
+        System.out.println("125*24"+"="+(numberone*numbertwo));
+        System.out.println("125/24"+"="+(numberone/numbertwo));
+        System.out.println("125mod24"+"="+(numberone%numbertwo));
+    }
+}
+
