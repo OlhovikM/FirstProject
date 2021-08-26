6d84ab4cc5cc36ef00a0d7e9ded7dc519d3660ae
OlhovikM <Olhovikmaria14@gmail.com>
2021-08-27 00:09:13 +0300

    Init

---
+++ com/company/mki8/MultiplicationTable.java
@@ -0,0 +1,13 @@
+package com.company.mki8;
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        printTable();
+    }
+
+    public static void printTable() {
+        for (int numb = 1; numb <= 10; numb++) {
+           int numb1=8;
+                System.out.println(numb+"x"+numb1+"="+(numb*numb1));
+            }
+        }
+    }
