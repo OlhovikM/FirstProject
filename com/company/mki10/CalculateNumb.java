3a44b078c1d2b6611410da12c7d575613484fdb1
OlhovikM <Olhovikmaria14@gmail.com>
2021-09-21 21:45:45 +0300

    Init

--- com/company/mki10/CalculateNumb.java
+++ com/company/mki10/CalculateNumb.java
@@ -6,6 +6,6 @@ public class CalculateNumb {
     public static void printNumb(int numberOne, int numberTwo, int numberThree) {
         int max = Math.max(Math.max(numberOne, numberTwo), numberThree);
         int min = Math.min(Math.min(numberOne, numberTwo), numberThree);
-        System.out.println("Mid of numbers 5, 10 and 15="+ (numberOne+numberTwo+numberThree-(min+max)));
+        System.out.println("Mid of numbers:" +" "+ numberOne+ " " +numberTwo +" "+numberThree + "="+ (numberOne+numberTwo+numberThree-(min+max)));
     }
 }
